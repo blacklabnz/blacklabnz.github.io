e0e833e9d789167d6e41fe73c370bdb1133e624c
blacklabnz <zufengxu@hotmail.com>
2022-04-05 14:40:34 +1200

    add utterance
    1. add utterance comments
    2. add javascript to load utterance theme base site theme

---
+++ themes/PaperMod/assets/js/utterance-theme.js
@@ -0,0 +1,37 @@
+var container = document.getElementById("page-comments");
+var prefTheme = localStorage.getItem("pref-theme");
+if (container !== null) {
+    var script = container.getElementsByTagName('script')[0]
+
+    if (script !== null) {
+        if (prefTheme === "dark") {
+            script.setAttribute("theme", "photon-dark");
+        }
+        else if (prefTheme === "light") {
+            script.setAttribute("theme", "github-light");
+        }
+    }
+}
+
+window.onclick = function (event) {
+
+    var container = document.getElementsByClassName("utterances-frame")[0];
+    var utteranceUrl = container.getAttribute('src');
+    var prefTheme = localStorage.getItem("pref-theme");
+    regex = new RegExp('(?<=&theme=).*?(?=&)');
+
+    if (event.target.matches("#theme-toggle") ||
+        event.target.parentNode.matches("#theme-toggle") ||
+        event.target.parentNode.matches("#moon") ||
+        event.target.parentNode.matches("#sun")) {
+
+        if (prefTheme === "dark") {
+            var newUtteranceUrl = utteranceUrl.replace(regex, "photon-dark");
+        }
+        else if (prefTheme === "light") {
+            var newUtteranceUrl = utteranceUrl.replace(regex, "github-light")
+        }
+
+        container.setAttribute("src", newUtteranceUrl);
+    }
+}
