2b99e1582b4f5903dc2e3ceea76857028d81df56
blacklabnz <zufengxu@hotmail.com>
2022-04-10 21:58:16 +1200

    fix menu not working in single page

--- themes/PaperMod/assets/js/menu.js
+++ themes/PaperMod/assets/js/menu.js
@@ -1,18 +1,19 @@
-window.onclick = function(event) {
+var dropbtn = document.getElementsByClassName("dropbtn")[0];
+if (dropbtn != null) {
+  dropbtn.addEventListener("click", () => { document.getElementById("dropdonwBox").classList.toggle("show"); });
+}
 
-  if (event.target.matches('.dropbtn') || 
-     event.target.parentNode.matches('.dropbtn')
-  ) {
-   document.getElementById("dropdonwBox").classList.toggle("show");
+window.addEventListener("click", (event) => {
+  if (event.target.matches("i.fas.fa-bars")) {
   }
   else {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var dropdownMenu = document.getElementsByClassName("dropdown-content");
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+    for (i = 0; i < dropdownMenu.length; i++) {
+      var openDropdown = dropdownMenu[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
-  }  
-}
+  } 
+})
